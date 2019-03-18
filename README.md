1f9e9a91b426d6903ac9da0488c8b51afd12a366
Chris Tomaszewski <christomaszewski@gmail.com>
2019-03-18 16:34:54 -0400

    First commit setting up respository.

---
+++ README.md
@@ -0,0 +1,24 @@
+## robot_utils
+
+A python library defining useful utilities commonly used in robotic applications (e.g. path planners, visualization, etc.)
+
+## Code Example
+
+A minimal usage example forthcoming.
+
+```python
+import robot_utils as rut
+
+```
+
+## Installation
+
+To install run:
+
+```
+python setup.py install
+```
+or 
+```
+python setup.py develop
+```
---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "robot_utils",
+    version = "0.0.1",
+    author = "Christopher 'ckt' Tomaszewski",
+    author_email = "christomaszewski@gmail.com",
+    description = ("A library of common utilities used in robotics"),
+    license = "BSD",
+    keywords = "robotics utilities",
+    url = "https://github.com/christomaszewski/robot_utils.git",
+    packages=['robot_utils', 'tests'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
