03fdd4e39d66928881d2a70cfa457a2284eb10a7
Chris Tomaszewski <christomaszewski@gmail.com>
2020-09-20 15:39:32 -0400

    Exposed tile source in MapView object through map_source argument. Added support for GoogleTiles

--- robot_utils/plotting.py
+++ robot_utils/plotting.py
@@ -6,14 +6,14 @@ import itertools
 import shapely
 import cartopy
 import cartopy.crs as ccrs
-from cartopy.io.img_tiles import MapboxTiles
+from cartopy.io.img_tiles import MapboxTiles, GoogleTiles
 
 class MapView(object):
 	""" A class for plotting various primitives on top of a map """
 
 	api_key = 'pk.eyJ1IjoiY2hyaXN0b21hc3pld3NraSIsImEiOiJjanJtN2h1OTAwZ2lnM3ltdDBmZDFjc3FyIn0.2i83Ad3s4mi9DR6ZLG-CFg'
 
-	def __init__(self, bounds, title='untitled', style='terrain-rgb', pause=0.00001, z_level=17):
+	def __init__(self, bounds, title='untitled', style='terrain-rgb', pause=0.00001, map_source='mapbox', z_level=17):
 		self._bounds = bounds
 		x_min, y_min, x_max, y_max = bounds
 
@@ -26,8 +26,11 @@ class MapView(object):
 		self._title = title
 		self._show_axes = True
 
-		self._imagery = MapboxTiles(MapView.api_key, style)
-		self._imagery._image_url = self._image_url
+		if map_source == 'google':
+			self._imagery = GoogleTiles()
+		else:
+			self._imagery = MapboxTiles(MapView.api_key, style)
+			self._imagery._image_url = self._image_url
 
 		self._ax = self._fig.add_subplot(1,1,1, projection=self._imagery.crs)
 		extent_buffer = 10
